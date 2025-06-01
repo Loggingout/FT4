7b463ae56440bd8b7fcb5634ebcb0566eaa64c26
Loggingout <167272973+Loggingout@users.noreply.github.com>
2025-05-31 19:02:42 -0600

    Imported & Initialized popover in js

--- FT4/src/js/main.js
+++ FT4/src/js/main.js
@@ -3,3 +3,12 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
+
+// Import popover
+import { Popover } from 'bootstrap';
+
+// Initialize popover
+const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new Popover(popoverTriggerEl)
+})
